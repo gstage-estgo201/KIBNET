d946d0bacd108df9cd6a6687dfad3ada48bba370
estgo201 <estgo201@greenstage.co.kr>
2018-01-15 18:08:20 +0900

    modify

--- src/test/java/services_/SpecificationNotificationTests.java
+++ src/test/java/services_/SpecificationNotificationTests.java
@@ -2,12 +2,9 @@ package services_;
 
 import org.junit.Test;
 
-import services_deposit.SpecificationNotification;
-
 public class SpecificationNotificationTests {
 	@Test
 	public void test() {
-		SpecificationNotification noti = new SpecificationNotification();
-		noti.Method();
+
 	}
 }
